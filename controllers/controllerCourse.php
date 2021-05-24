1a8cb9e23a9c4b82f21386254d98b2aeb1940191
Mitch.D <michel-dufour@neuf.fr>
2021-05-24 23:15:50 +0200

    Add function getAllCourses dans controllerCourse.php
    Ajout de la fonction getAllCourses dans le controller pour qu'il récupère tous les cours de la base de données

--- controllers/controllerCourse.php
+++ controllers/controllerCourse.php
@@ -1,9 +1,11 @@
-<!-- controller d'ajout d'un nouveau cours -->
+<!-- controller pour les cours -->
 <?php
 
 require_once 'models/modelCourses.php';
 
-//fonction qui ajoute un cours
+/**
+ * Fonction qui permet d'ajouter un cours dans la base de données
+ */
 function addOneCourse(){
 
     if(isset($_POST['enregistrer'])){
@@ -20,5 +22,28 @@ function addOneCourse(){
     }
     //on a besoin de notre view
     require_once 'views/viewCourse.php';
+}
 
+/**
+ * Fonction qui permet de récupérer tous les cours de la base de données
+ */
+function getAllCourses(){
+    $resultGetCourses = getCourses();
+
+    if(!$resultGetCourses){
+        $message = "La récupération des cours a échoué !";
+    }
+    else {
+        $nb_courses = $resultGetCourses->rowCount();
+        if($nb_courses == 0){
+            $message = "Il n'y a aucun cours pour le moment !";
+            //on redirige vers le formulaire d'ajout de cours
+            addOneCourse();
+        } else {
+            //page d'affichage de tous les cours
+            require_once 'views/viewAllCourses.php';
+        }
+    }
+    //on ferme la connexion du serveur pour permettre a d'autres requete de s'executer
+    $resultGetCourses->closeCursor();
 }
