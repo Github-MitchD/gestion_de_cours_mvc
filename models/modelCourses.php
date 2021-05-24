d659da912273e383199317ebadf1de196d9c3d66
Mitch.D <michel-dufour@neuf.fr>
2021-05-24 23:14:36 +0200

    Add fonction getCourses dans modelCourses.php
    Ajout de la fonction getCourses qui permet au modelCourses de récupérer tous les cours de la base de données

--- models/modelCourses.php
+++ models/modelCourses.php
@@ -1,19 +1,28 @@
-<!-- model d'ajout d'un nouveau cours -->
+<!-- model pour les cours -->
 <?php
 
 //on appel la connection a la base de données
 require_once 'models/model.php';
 
-//fonction qui permet d'ajouter un cours
+//fonction qui permet d'ajouter un cours en bdd
 function addCourse($courseCode, $courseTitle, $courseLangage){
     //on se connecte a la base de donnees qu'on met dans variable $bddPDO
-    $dbbPDO = connexionDBB();
+    $bddPDO = connexionBDD();
 
-    $requete = $dbbPDO->prepare('INSERT INTO courses(courseCode, courseTitle, courseLangage) VALUES (:courseCode, :courseTitle, :courseLangage)');
+    $requete = $bddPDO->prepare('INSERT INTO courses(courseCode, courseTitle, courseLangage) VALUES (:courseCode, :courseTitle, :courseLangage)');
     $requete->bindValue(':courseCode', $courseCode);
     $requete->bindValue(':courseTitle', $courseTitle);
     $requete->bindValue(':courseLangage', $courseLangage);
 
     $result = $requete->execute();
     return $result;
+}
+
+//fonction qui permet de recuperer les cours en bdd
+function getCourses(){
+    //on se co a la base de données
+    $bddPDO = connexionBDD();
+    $requete = "SELECT * FROM courses ORDER BY coursesId ASC";
+    $resultGetCourses = $bddPDO->query($requete);
+    return $resultGetCourses;
 }
