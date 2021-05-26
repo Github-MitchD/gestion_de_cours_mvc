f7fe2ff3cd3d1cf2f74a7bd5e3164f3c22d0d8ae
Mitch.D <michel-dufour@neuf.fr>
2021-05-27 00:48:41 +0200

    Add fonction getStudentWithMail dans le modelStudent

--- models/modelStudents.php
+++ models/modelStudents.php
@@ -8,20 +8,38 @@ require_once 'models/model.php';
  * @param $studentINE, $firstName, $lastName, $adress, $city, $email, $password
  * @return $resultAddStudent
  */
-function addStudent($studentINE, $firstName, $lastName, $adress, $city, $email, $password){
+function addStudent($studentINE, $firstName, $lastName, $adress, $city, $mail, $password){
     //on initialise la base de données
     $bddPDO = connexionBDD();
-    $requete = $bddPDO->prepare('INSERT INTO students(studentINE, firstName, lastName, adress, city, email, password) VALUES (:studentINE, :firstName, :lastName, :adress, :city, :email, :password)');
+    $requete = $bddPDO->prepare('INSERT INTO students(studentINE, firstName, lastName, adress, city, mail, password) VALUES (:studentINE, :firstName, :lastName, :adress, :city, :mail, :password)');
 
     $requete->bindValue(':studentINE', $studentINE);
     $requete->bindValue(':firstName', $firstName);
     $requete->bindValue(':lastName', $lastName);
     $requete->bindValue(':adress', $adress);
     $requete->bindValue(':city', $city);
-    $requete->bindValue(':email', $email);
+    $requete->bindValue(':mail', $mail);
     $requete->bindValue(':password', $password);
 
     $resultAddStudent = $requete->execute();
     
     return $resultAddStudent;
+}
+
+/**
+ * Fonction qui verifie si l'email est déjà existant dans la bdd
+ * @param $mail
+ * @return $data
+ */
+function getStudentWithMail($mail){
+    //on initialise la base de données
+    $bddPDO = connexionBDD();
+
+    $requete = "SELECT * FROM students WHERE mail = '$mail'";
+    $result = $bddPDO->query($requete);
+
+    //on recupere dans un tableau associatif
+    $data = $result->fetch(PDO::FETCH_ASSOC);
+
+    return $data;
 }
